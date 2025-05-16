5aa0ba53c4123238917fb0fa964c09fd3d5e9106
Seth Boyles <seth.boyles@broadcom.com>
2025-05-16 16:45:44 -0700

    root route

--- app/controllers/capi_engine/application_controller.rb
+++ app/controllers/capi_engine/application_controller.rb
@@ -1,4 +1,7 @@
 module CapiEngine
   class ApplicationController < ActionController::API
+    def root
+      render status: :ok
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 CapiEngine::Engine.routes.draw do
+  get '/', to: 'application#root'
   delete '/apps/:guid', to: "special_app_delete#delete"
 end
