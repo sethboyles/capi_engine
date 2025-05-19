69d76b19be832ff7433704336d83eb46c7f20db4
Seth Boyles <seth.boyles@broadcom.com>
2025-05-19 15:27:59 -0700

    bump version

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,4 @@ CapiEngine::Engine.routes.draw do
   get '/', to: 'special_app_delete#root'
   delete '/apps/:guid', to: "special_app_delete#delete"
 end
+binding.pry
--- lib/capi_engine/engine.rb
+++ lib/capi_engine/engine.rb
@@ -1,5 +1,6 @@
 module CapiEngine
   class Engine < ::Rails::Engine
+    # engine_name "capi_engine"
     isolate_namespace CapiEngine
     config.generators.api_only = true
   end
