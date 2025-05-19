063e79fd06fd52168f0366d77186a41c37ccac1e
Seth Boyles <seth.boyles@broadcom.com>
2025-05-19 15:49:38 -0700

    remove routes require

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,3 @@ CapiEngine::Engine.routes.draw do
   get '/', to: 'special_app_delete#root'
   delete '/apps/:guid', to: "special_app_delete#delete"
 end
-binding.pry
--- lib/capi_engine.rb
+++ lib/capi_engine.rb
@@ -1,6 +1,6 @@
 require "capi_engine/version"
 require "capi_engine/engine"
-require 'config/routes'
+
 
 module CapiEngine
   # Your code goes here...
